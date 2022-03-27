04596b64306ceb01eb8d2bf8d924616293829a42
aleks802 <aleks.marchenko802@gmail.com>
2022-03-27 19:38:05 +0300

    hw_01

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -2,29 +2,61 @@
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private int capacity = 3;
+    Resume[] storage = new Resume[capacity];
+    private int size = 0;
 
     void clear() {
+        for (int i = 0; i < size; i++) {
+                storage[i] = null;
+            }
+        size = 0;
     }
 
     void save(Resume r) {
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume resume = null;
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("Resume " + uuid + " is not exist");
+        } else {
+            resume = storage[index];
+        }
+        return resume;
     }
 
     void delete(String uuid) {
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("Resume " + uuid + " is not exist");
+        } else {
+            storage[index] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return storage;
     }
 
     int size() {
-        return 0;
+        return size;
+    }
+
+    private int getIndex(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid == storage[i].uuid) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
