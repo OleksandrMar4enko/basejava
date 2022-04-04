6526c4ecc8b7f4d5056e45f0a0e1ac3b0a984b02
aleks802 <aleks.marchenko802@gmail.com>
2022-04-04 22:12:24 +0300

    hw 03

--- src/MainTestArrayStorage.java
+++ src/MainTestArrayStorage.java
@@ -17,6 +17,8 @@ public class MainTestArrayStorage {
 
         ARRAY_STORAGE.save(r1);
         ARRAY_STORAGE.save(r2);
+        r2.setUuid("uuid4");
+        ARRAY_STORAGE.update(r2);
         ARRAY_STORAGE.save(r3);
 
         System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.getUuid()));
