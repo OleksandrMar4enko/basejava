0aa890853ac39503fef858d3b20f7db833d687fb
aleks802 <aleks.marchenko802@gmail.com>
2022-05-06 23:40:13 +0300

    Lesson08 HW08

--- src/ru/javawebinar/basejava/exception/StorageException.java
+++ src/ru/javawebinar/basejava/exception/StorageException.java
@@ -3,8 +3,8 @@ package ru.javawebinar.basejava.exception;
 public class StorageException extends RuntimeException {
 	private final String uuid;
 
-	public StorageException(String message, String uuid) {
-		super(message);
+	public StorageException(String message, String uuid, Exception e) {
+		super(message, e);
 		this.uuid = uuid;
 	}
 
